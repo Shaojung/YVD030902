c6abbc174fe9b44a6f6aa8229f1c873e79e44018
Shaojung Lu <shaojung@gmail.com>
2017-03-09 10:21:38 +0800

    progresbar

--- app/src/main/java/com/example/yvtc/yvd030902/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvd030902/MainActivity.java
@@ -6,12 +6,13 @@ import android.view.View;
 import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
-    ProgressBar pb;
+    ProgressBar pb, pb2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         pb = (ProgressBar) findViewById(R.id.progressBar);
+        pb2 = (ProgressBar) findViewById(R.id.progressBar2);
     }
     public void click1(View v)
     {
@@ -21,4 +22,10 @@ public class MainActivity extends AppCompatActivity {
     {
         pb.setVisibility(View.INVISIBLE);
     }
+
+    public void click3(View v)
+    {
+        pb2.setProgress(pb2.getProgress() + 10);
+    }
+
 }
