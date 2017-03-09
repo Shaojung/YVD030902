154f21a07bd55f846be3f5b12ed2c5698612aa1a
Shaojung Lu <shaojung@gmail.com>
2017-03-09 11:00:40 +0800

    SeekBar

--- app/src/main/java/com/example/yvtc/yvd030902/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvd030902/MainActivity.java
@@ -2,17 +2,38 @@ package com.example.yvtc.yvd030902;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
     ProgressBar pb, pb2;
+    SeekBar sb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         pb = (ProgressBar) findViewById(R.id.progressBar);
         pb2 = (ProgressBar) findViewById(R.id.progressBar2);
+        sb = (SeekBar) findViewById(R.id.seekBar);
+
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                Log.d("SB", "" + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
     public void click1(View v)
     {
